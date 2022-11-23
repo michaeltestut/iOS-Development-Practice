da5e6854b04f5db4691f2958bf0db225e548ec4b
Michael Testut <michael.testut@yahoo.com>
2022-11-23 11:19:47 -0800

    new project - I am poor

---
+++ I_Am_Poor/I_Am_Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_Am_Poor
+//
+//  Created by Michael Testut on 11/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
