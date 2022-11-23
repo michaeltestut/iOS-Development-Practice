4405d6153bdf6319dd6971fd6787e18a906db307
Michael Testut <michael.testut@yahoo.com>
2022-11-22 17:54:15 -0800

    added I_am_Rich project

---
+++ I_am_Rich/I_am_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_am_Rich
+//
+//  Created by Michael Testut on 11/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
