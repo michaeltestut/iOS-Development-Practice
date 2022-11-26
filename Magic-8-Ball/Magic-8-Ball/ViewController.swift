6c5668af3890ec11571ca4aadb5aa05fef5dc271
Michael Testut <michael.testut@yahoo.com>
2022-11-25 22:27:22 -0800

    added functionality

--- Magic-8-Ball/Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/Magic-8-Ball/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var magic8Ball: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func change8BallImage(_ sender: Any) {
+        let magicBallImage = [
+        UIImage(imageLiteralResourceName: "ball1"),
+        UIImage(imageLiteralResourceName: "ball2"),
+        UIImage(imageLiteralResourceName: "ball3"),
+        UIImage(imageLiteralResourceName: "ball4"),
+        UIImage(imageLiteralResourceName: "ball5"),
+        ]
+        
+        magic8Ball.image=magicBallImage.randomElement();
+        
+    }
+    
 }
 
