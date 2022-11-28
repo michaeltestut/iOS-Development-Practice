322b32b67237fe3ce654459e2059ad63e1607e4f
Michael Testut <michael.testut@yahoo.com>
2022-11-27 21:09:47 -0800

    completed xylophone project

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -2,11 +2,11 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
 
@@ -14,7 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    func playSound(noteToPlay:String) {
+        guard let url = Bundle.main.url(forResource: noteToPlay, withExtension: "wav") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(noteToPlay:sender.currentTitle!);
+    }
     
 
 }
