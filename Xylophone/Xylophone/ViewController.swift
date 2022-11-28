c61d128d86886c9247085640d0e4dde6f445a518
Michael Testut <michael.testut@yahoo.com>
2022-11-27 21:15:31 -0800

    added additional styling/functionality to xylophone

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -32,6 +32,11 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            sender.alpha = 1.0
+        }
         playSound(noteToPlay:sender.currentTitle!);
     }
     
