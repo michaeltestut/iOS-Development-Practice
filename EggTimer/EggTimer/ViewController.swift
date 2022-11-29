84d99e742e3773683fa53c79f514346dfa0298cc
Michael Testut <michael.testut@yahoo.com>
2022-11-28 18:38:08 -0800

    started timer functionality

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
+let eggTime = ["Soft":5, "Medium":8, "Hard":12]
+var counter = 30
+
+var timer = Timer()
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle;
+        counter = eggTime[hardness!]!
+        timer=Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds")
+            label.text="\(counter) seconds"
+            counter -= 1
+        }
+        else{
+            timer.invalidate()
+            label.text="Done!"
+        }
+    }
 }
