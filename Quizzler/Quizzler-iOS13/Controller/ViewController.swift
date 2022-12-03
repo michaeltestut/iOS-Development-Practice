a923527c51366e3bd608ad606031c3f4c480949a
Michael Testut <michael.testut@yahoo.com>
2022-12-02 22:36:18 -0800

    began code restructuring

---
+++ Quizzler/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizLogic = QuizLogic()
+    var timer=Timer()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer=sender.currentTitle
+        let checkUserAnswer = quizLogic.checkAnswer(userAnswer!)
+        if checkUserAnswer{
+            sender.backgroundColor=UIColor.green
+        }
+        else{
+            sender.backgroundColor=UIColor.red
+        }
+        timer=Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI(){
+        trueButton.backgroundColor=UIColor.clear
+        falseButton.backgroundColor=UIColor.clear
+        questionLabel.text=quizLogic.getQuestionText()
+        progressBar.progress=quizLogic.getProgress()
+        quizLogic.nextQuestion()
+     
+    }
+}
+
