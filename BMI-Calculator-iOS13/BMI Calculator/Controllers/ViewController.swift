89e48a6dfc4174b70b74e474e226d9eeae7a281d
Michael Testut <michael.testut@yahoo.com>
2022-12-14 11:27:48 -0800

    updated sliders

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -7,11 +7,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightValue: UILabel!
+    
+    @IBOutlet weak var weightValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightValue.text=String(format:"%.2f", sender.value)+"m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValue.text=String(format:"%.0f",sender.value)+"Kg"
+    }
+    
+    
+    
 }
 
