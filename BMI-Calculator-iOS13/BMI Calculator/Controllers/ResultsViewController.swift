593a168159035e18b0d03e2865f0d1e191953599
Michael Testut <michael.testut@yahoo.com>
2022-12-16 21:33:28 -0800

    second view added

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Michael Testut on 12/16/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
