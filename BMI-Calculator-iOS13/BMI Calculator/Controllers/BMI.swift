1e1ea700248a32407a199de5a1ebfecb449424b6
Michael Testut <michael.testut@yahoo.com>
2022-12-19 21:58:20 -0800

    updated UI

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Michael Testut on 12/18/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+struct BMI{
+    let value:Float
+    let advice:String
+    let color:UIColor
+}
--- BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,17 +6,24 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain{
-    var bmi:Float?
+    var bmi:BMI?
     mutating func calculateBMI(height:Float,weight:Float){
-        bmi=weight/pow(height, 2)
-        
+        let bmiValue=weight/pow(height, 2)
+        if bmiValue<18.5{
+            bmi=BMI(value: bmiValue, advice: "Eat More Food Please", color: UIColor.blue)        }
+        else if bmiValue<24.9{
+            bmi=BMI(value: bmiValue, advice: "Keep it up!", color: UIColor.green)
+        }
+        else{
+            bmi=BMI(value: bmiValue, advice: "Eat more veggies and exercise!", color: UIColor.red)
+        }
     }
     
     func getBMIValue()->String{
-        let bmiValue=String(format:"%.1f",bmi ?? 0.0)
+        let bmiValue=String(format:"%.1f",bmi?.value ?? 0.0)
         return bmiValue
     }
 }
