ed3244b93c69b278304548e0b1fad2531ae15c30
Michael Testut <michael.testut@yahoo.com>
2022-12-22 14:40:32 -0800

    IBActions/Outlets added

--- Tipsy-iOS13/Tipsy/ViewController.swift
+++ Tipsy-iOS13/Tipsy/ViewController.swift
@@ -7,11 +7,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var splitNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
 }
 
