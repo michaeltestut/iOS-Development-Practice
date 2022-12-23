dad5694fdca353fb61ee4b4fb130ea958e1c74c7
Michael Testut <michael.testut@yahoo.com>
2022-12-23 00:23:53 -0800

    Results Screen updated

--- Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
@@ -5,18 +5,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
-    
     @IBOutlet weak var splitNumberLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
