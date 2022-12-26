5c910cfa4fdec66daae597c86a0f674e4370ad35
Michael Testut <michael.testut@yahoo.com>
2022-12-26 11:23:17 -0600

    updated functionality

--- Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
@@ -14,14 +14,26 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     
     var tipPercentage = 0.1;
+    var stepperValue=2
+    var perPersonTotal=0
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        billTextField.endEditing(true)
+        stepperValue=Int(sender.value)
+        splitNumberLabel.text=String(stepperValue)
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        var billTotal = Double(billTextField.text!)!
+        var split = ((billTotal*tipPercentage)+billTotal)/Double(stepperValue)
         print(tipPercentage)
+        print(stepperValue)
+        print(billTextField.text!)
+        print (round(split*100)/100)
     }
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
         zeroPctButton.isSelected=false;
         tenPctButton.isSelected=false;
         twentyPctButton.isSelected=false;
