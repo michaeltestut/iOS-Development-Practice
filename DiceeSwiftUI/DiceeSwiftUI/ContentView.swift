c7d17fc16ec56754b36103c5ef5ebb50f7eb05e7
Michael Testut <michael.testut@yahoo.com>
2023-01-30 15:10:50 -0800

    new project

---
+++ DiceeSwiftUI/DiceeSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceeSwiftUI
+//
+//  Created by Michael Testut on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceeSwiftUI/DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Michael Testut on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
